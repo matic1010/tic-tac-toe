b3468048eabc6eaa3c1373f64b66a8c069859282
Nils Matic <matic1909@gmail.com>
2023-05-05 15:07:21 +0200

    added gameController and displayController

--- index.js
+++ index.js
@@ -20,3 +20,43 @@ const gameBoard = (() => {
     setField,
   };
 })();
+
+const gameController = (() => {
+  const playerX = Player('X');
+  const playerO = Player('O');
+  let _currentPlayer = playerX;
+
+  const getCurrentPlayer = () => _currentPlayer;
+  const changeCurrentPlayer = () => {
+    if (_currentPlayer === playerX) {
+      _currentPlayer = playerO;
+    } else {
+      _currentPlayer = playerX;
+    }
+  };
+
+  const makeMove = (index) => {
+    gameBoard.setField(index, _currentPlayer);
+    changeCurrentPlayer();
+  };
+
+  return { makeMove };
+})();
+
+const displayController = (() => {
+  let cells = document.querySelectorAll('.cell');
+
+  cells.forEach((cell) => {
+    cell.addEventListener('click', (e) => {
+      const index = parseInt(e.target.dataset.index);
+      gameController.makeMove(index);
+      _updateBoard();
+    });
+  });
+
+  const _updateBoard = () => {
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].textContent = gameBoard.getField(i);
+    }
+  };
+})();
