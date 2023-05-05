e6a33173cb6326ccffc205eea43fd40795aaff9d
Nils Matic <matic1909@gmail.com>
2023-05-05 14:30:56 +0200

    added Player factory and GameBoard controller

---
+++ index.js
@@ -0,0 +1,22 @@
+const Player = function (sign) {
+  let _sign = sign;
+
+  const getSign = () => _sign;
+  const setSign = (sign) => (_sign = sign);
+
+  return { getSign, setSign };
+};
+
+const GameBoard = (function () {
+  let _board = new Array(9);
+
+  const getField = (index) => _board[index];
+  const setField = (index, player) => {
+    _board[index] = player.getSign();
+  };
+
+  return {
+    getField,
+    setField,
+  };
+})();
